22f3b5d6f91535b84936187eac0f78064f6ad0f5
Andrew Aliaj <andyaliaj@gmail.com>
2024-08-22 09:21:33 -0400

    updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,18 @@
-import type { Config } from "tailwindcss";
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.{css,scss,sass,less,styl,stylus}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#ff0000",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
