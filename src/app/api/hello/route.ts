1475076c0d3749a98a00650c44a7e4e1aecce3d0
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-03 17:12:28 -0400

    hello

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,3 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function handler(req: NextRequest) {
-  return NextResponse.json({ message: "Hello from route.ts" });
-}
+console.log("Hello from route.ts");
