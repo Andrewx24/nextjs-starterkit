19383140cbb9a87477efd1a78b5f44891ed43c78
Andrew Aliaj <andyaliaj@gmail.com>
2024-10-24 12:15:03 -0400

    updates

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function AdminPage() {
+    return (
+        <div>
+        <h1 className="text-3xl font-semibold mb-4">Admin</h1>
+        <p className="mb-4">
+            This page is an example of an admin page. Admin pages are pages that are only accessible to users with admin privileges. This page demonstrates how to create an admin page with Next.js.
+        </p>
+
+        
+        </div>
+    );
+}
---
+++ src/app/(admin)/cms/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function CMSBlogPage() {
+    return (
+        <div>
+        <h1 className="text-3xl font-semibold mb-4">CMS Blog</h1>
+        <p className="mb-4">
+            This page is an example of a CMS blog. CMS blogs are blogs that are created using a content management system. This page demonstrates how to create a CMS blog with Next.js.
+        </p>
+
+        
+        </div>
+    );
+}
